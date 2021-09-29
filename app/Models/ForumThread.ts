3af4a6a79dc9f6ef13427d31c77ad04006e6955e
ShigeroToura <notshigetora@gmail.com>
2021-09-29 20:59:14 +0200

    Added user to forum model as belongTo

--- app/Models/ForumThread.ts
+++ app/Models/ForumThread.ts
@@ -1,13 +1,16 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
 import ForumUpvote from 'App/Models/ForumUpvote'
+import User from 'App/Models/User'
 
 export default class ForumThread extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column()
-  public userID: number
+  @belongsTo(() => User, {
+    foreignKey: 'userID',
+  })
+  public userID: BelongsTo<typeof User>
 
   @column()
   public threadTitle: string
